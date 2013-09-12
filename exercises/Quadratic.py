a729b9f42c96654232a918aa671cf9427bfd9738
daveagp <daveagp@gmail.com>
2013-09-12 12:16:57 -0400

    floating-point comparison change in Quadratic:wq

--- exercises/Quadratic.py
+++ exercises/Quadratic.py
@@ -1,4 +1,5 @@
 description = """
+<p>
 Given command-line arguments
 <code>b</code> and <code>c</code>, print out the 
 roots of <code>x<sup>2</sup> + bx + c = 0</code>. They are given by
@@ -7,8 +8,10 @@ the formula
 <img style='width:150px' src='exercises/Quadratic.png'></div>
 If the <b>discriminant</b> <code>b<sup>2</sup>-4c</code> is 
 negative, instead print out <code>There are no roots</code> and
-if the discriminant is zero then print out only one root (since they
+if the discriminant is zero* then print out only one root (since they
 are the same). When there are two roots, print the <b>smaller</b> one first.
+<p>
+*: Because of floating-point inaccuracy, it's better to check if the discriminant is very close to zero, say within 10<sup>-6</sup>. You could use a condition like <code>Math.abs(disc) < 1E-6</code>.
 """
 
 source_code = r"""
@@ -19,7 +22,7 @@ public static void main(String[] args) {
    double disc = (b*b-4*c);
    double root1 = (-b-Math.sqrt(disc))/2;
    double root2 = (-b+Math.sqrt(disc))/2;
-   if (disc == 0)
+   if (Math.abs(disc) < 1E-6)
       System.out.println(root1);
    else if (disc < 0)
       System.out.println("There are no roots");
@@ -37,5 +40,6 @@ testMain(-1, -1);
 testMain(0, -2);
 testMain(0, 2);
 testMain(-4, 4);
+testMain(-0.1, 0.0025);
 testMain(randgen.nextInt(10)+10, randgen.nextInt(20));
 """
