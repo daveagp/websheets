cf162fde3e03a360af627ac298224d904afcf8d4
COS126 at Princeton <cos126@cs.princeton.edu>
2013-09-24 14:50:40 -0400

    array exercises

---
+++ exercises/Distinct.py
@@ -0,0 +1,44 @@
+description = r"""
+Write a program <code>Distinct</code> that takes an arbitrary
+number of integer command-line arguments; it should print
+<code>true</code> if they all have distinct values, and 
+<code>false</code> otherwise."""
+
+source_code = """
+public class Distinct {
+   public static void main(String[] args) {
+ 
+      int N = args.length;    
+         
+      // convert each arg and store them in an array of integers
+      int[] values =\[ new int[N] ]\;
+      for (int i = 0; \[i < N]\; \[i += 1]\)
+          \[ values[i] ]\ = Integer.parseInt(args[i]);    
+        
+      // are all of the pairs examined so far distinct?
+      boolean result = true;
+
+      // we'll examine each values[i] in the array 
+      for (int i = 0; i < N; i++) {
+         // we'll examine values[j] for each other j
+         for (int j =\[i+1]\; \[j < N]\; \[j++]\) {
+            // are they different or not?
+            if (\[values[i] == values[j]]\) {
+               result =\[false]\;
+            }
+         }
+      }
+         
+      System.out.println(result);
+   }
+}
+"""
+
+tests = r"""
+testMain(11, 23, -7, 0, 99, 5, 42);
+testMain(2, 4, 6, 3, 6);
+testMain(-3, -2, -1, -0, +3, +2, +1, +0);
+testMain(2, 3, -3, -2);
+testMain(126);
+testMain();
+"""
---
+++ exercises/NSwap.py
@@ -0,0 +1,54 @@
+source_code = r"""
+public static void main(String[] args) {
+   // the grader will pre-define an array of color names
+\fake[
+
+   String[] colors = ...; // e.g. new String[] {"red", "blue", "green"};
+]\
+\hide[
+   String[] colors = args;
+]\
+
+   int n = colors.length; // for convenience
+
+   System.out.println("Before n-swapping:");
+   for (int i=0; i<n; i++)
+      System.out.println("color " + i + " is " + colors[i]);
+ 
+   // your swapping code here:
+\[
+   String tmp = colors[n-1];
+   for (int i=n-1; i>=1; i--)
+      colors[i] = colors[i-1];
+   colors[0] = tmp;
+]\
+   System.out.println("After n-swapping:");
+   for (int i=0; i<n; i++)
+      System.out.println("color " + i + " is " + colors[i]);
+}
+"""
+
+tester_preamble = r"""
+class OurTestCase extends BasicTestCase {
+   OurTestCase(String... S ) {super("main", new Object[]{S});}
+   protected void describe() {
+   System.out.print("Running your code on a test case");
+}
+}
+"""
+
+tests = r"""
+  new OurTestCase("red", "green", "blue").execute();
+  new OurTestCase("fuschia", "turquoise", "sienna", "vermilion").execute();
+  new OurTestCase("a", "b", "c", "d", "e", "f", "g", "h").execute();
+  new OurTestCase("monocolor").execute();
+//   test("fourSwap", "fuschia", "turquoise", "sienna", "vermilion");
+"""
+
+description = r"""
+This is an extension of the <b>SquareSwap</b> websheet.
+Given an array <code>colors</code> of <code>n</code> strings,
+move each string one position later in the array, except you
+must move the one at the end to the start.
+"""
+
