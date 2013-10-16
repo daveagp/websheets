04096306761fa8aa46d224ed1d0be5c220935ec8
COS126 at Princeton <cos126@cs.princeton.edu>
2013-10-15 23:17:23 -0400

    add favicon

--- test.php
+++ test.php
@@ -3,6 +3,9 @@ include_once('include.php');
 ?><html>
 <head>
    <title>Websheets</title>
+   <link rel="icon" 
+      type="image/png" 
+      href="favicon.png">
    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
    <script type="text/javascript" src="CodeMirror/lib/codemirror.js"></script>
    <script type="text/javascript" src="CodeMirror/mode/clike/clike.js"></script>
